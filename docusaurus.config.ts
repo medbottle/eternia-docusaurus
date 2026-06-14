94d0df84a010ee8da58423906aedd89494ecdc91
medbottle <cachorrogringo31@gmail.com>
2026-06-13 22:08:45 -0300

    Add Algolia search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,8 +35,6 @@ const config: Config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          
-
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -46,6 +44,38 @@ const config: Config = {
   ],
 
   themeConfig: {
+
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'W1FW1P39YD',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'f6d40494c221b2d951c0dce39d2b43e1',
+
+      indexName: 'Eternia Doc',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,  
+  },
+
     navbar: {
       title: 'Eternia Docs',
       logo: {
